241eb64ec15146c2b57dff2723aea64bfa367e30
happyaccidents <hello@happyaccidents.me>
2013-08-02 15:04:35 +0100

    ORNO-11 Added json file loader and tests

---
+++ src/Orno/Config/File/JsonFileLoader.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * The Orno Component Library
+ *
+ * @author  Phil Bennett @philipobenito
+ * @license http://www.opensource.org/licenses/mit-license.php MIT (see the LICENSE file)
+ */
+namespace Orno\Config\File;
+
+/**
+ * Json File Loader
+ */
+class JsonFileLoader extends AbstractFileLoader
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function parse()
+    {
+        $array = json_decode(file_get_contents($this->file), true);
+
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            throw new Exception\ParseException(
+                sprintf('The file (%s) must contain a valid JSON string', $this->file)
+            );
+        }
+
+        return (is_null($this->key)) ? $array : [$this->key => $array];
+    }
+}
--- tests/FileLoaderTest.php
+++ tests/FileLoaderTest.php
@@ -4,6 +4,7 @@ namespace OrnoTest;
 
 use Orno\Config\File\ArrayFileLoader;
 use Orno\Config\File\YamlFileLoader;
+use Orno\Config\File\JsonFileLoader;
 
 class FileLoaderTest extends \PHPUnit_Framework_Testcase
 {
@@ -73,14 +74,6 @@ class FileLoaderTest extends \PHPUnit_Framework_Testcase
         );
     }
 
-    public function testYamlFileLoaderThrowsExceptionWithMalformedFile()
-    {
-        $this->setExpectedException('Orno\Config\File\Exception\ParseException');
-
-        $loader = new YamlFileLoader(__DIR__ . '/assets/yaml-file-malformed.yml');
-        $array = $loader->parse();
-    }
-
     public function testParsesYamlFileWithKey()
     {
         $loader = new YamlFileLoader(__DIR__ . '/assets/yaml-file.yml', 'some-parent-key');
@@ -100,5 +93,61 @@ class FileLoaderTest extends \PHPUnit_Framework_Testcase
                 ]
             ]
         );
+    }
+
+    public function testYamlFileLoaderThrowsExceptionWithMalformedFile()
+    {
+        $this->setExpectedException('Orno\Config\File\Exception\ParseException');
+
+        $loader = new YamlFileLoader(__DIR__ . '/assets/yaml-file-malformed.yml');
+        $array = $loader->parse();
+    }
+
+    public function testParsesJsonFileWithoutKey()
+    {
+        $loader = new JsonFileLoader(__DIR__ . '/assets/json-file.json');
+        $array = $loader->parse();
+
+        $this->assertInternalType('array', $array);
+        $this->assertSame(
+            $array,
+            [
+                'SomeKey' => [
+                    'SomeChildKey' => [
+                        'key1' => 'value1',
+                        'key2' => 'value2'
+                    ]
+                ]
+            ]
+        );
+    }
+
+    public function testParsesJsonFileWithKey()
+    {
+        $loader = new JsonFileLoader(__DIR__ . '/assets/json-file.json', 'some-parent-key');
+        $array = $loader->parse();
+
+        $this->assertInternalType('array', $array);
+        $this->assertSame(
+            $array,
+            [
+                'some-parent-key' => [
+                    'SomeKey' => [
+                        'SomeChildKey' => [
+                            'key1' => 'value1',
+                            'key2' => 'value2'
+                        ]
+                    ]
+                ]
+            ]
+        );
+    }
+
+    public function testJsonFileLoaderThrowsExceptionWithMalformedFile()
+    {
+        $this->setExpectedException('Orno\Config\File\Exception\ParseException');
+
+        $loader = new JsonFileLoader(__DIR__ . '/assets/json-file-malformed.json');
+        $array = $loader->parse();
     }
 }
