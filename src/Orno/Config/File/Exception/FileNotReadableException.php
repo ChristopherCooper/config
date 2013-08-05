c0db816c531b27afcb35b67af6fec76df6c9f709
happyaccidents <hello@happyaccidents.me>
2013-08-05 15:45:42 +0100

    Changed file level doc blocks

--- src/Orno/Config/File/Exception/FileNotReadableException.php
+++ src/Orno/Config/File/Exception/FileNotReadableException.php
@@ -3,7 +3,7 @@
  * The Orno Component Library
  *
  * @author  Phil Bennett @philipobenito
- * @license http://www.opensource.org/licenses/mit-license.php MIT (see the LICENSE file)
+ * @license MIT (see the LICENSE file)
  */
 namespace Orno\Config\File\Exception;
 
